c982f6df89e6671781861a6cfd21398a363e29ff
mac <zhangkungg@163.com>
2020-05-12 19:19:29 +0800

    init

---
+++ precache-manifest.b1078e623bb114eacf2820c5b1832fb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9786936e09346516c0283f454581bd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba914cae7ba5cabea9c",
+    "url": "/static/css/main.0a23eb15.chunk.css"
+  },
+  {
+    "revision": "c1613b70a9a66fef5790",
+    "url": "/static/js/2.42832d1a.chunk.js"
+  },
+  {
+    "revision": "cba914cae7ba5cabea9c",
+    "url": "/static/js/main.403ab5b0.chunk.js"
+  },
+  {
+    "revision": "ac1571c73f3c302d6eb1",
+    "url": "/static/js/runtime-main.0518e3d4.js"
+  },
+  {
+    "revision": "783bb457ed9b3294fe72f4b4bc9358a9",
+    "url": "/static/media/Zpix.783bb457.ttf"
+  },
+  {
+    "revision": "0c20d7e1c9098197c09d2c6aaa46ab5b",
+    "url": "/static/media/akkarin.0c20d7e1.jpg"
+  },
+  {
+    "revision": "f8c7835648f9ac28c4e9d60b961ce90e",
+    "url": "/static/media/mashroom.f8c78356.png"
+  }
+]);
